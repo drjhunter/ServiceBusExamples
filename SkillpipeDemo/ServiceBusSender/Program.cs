01578ce3d23a70e8495977fb574b700eb4541b39
Julia Hunter <Julia.Hunter@ijyi.com>
2020-08-18 15:05:44 +0100

    Initial commit

---
+++ SkillpipeDemo/ServiceBusSender/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Azure.ServiceBus;
+
+namespace ServiceBusSender
+{
+    class Program
+    {
+        const string ServiceBusConnectionString = "Endpoint=sb://sampleservicebusjh.servicebus.windows.net/;SharedAccessKeyName=SenderPolicyScopedNS;SharedAccessKey=sJoIb3fROht8cCxJtW4Ja/y9TLSuscbYkEIakUC5akU=";
+        const string QueueName = "TestQueue";
+        static IQueueClient queueClient;
+
+        static async Task Main(string[] args)
+        {
+            const int numberOfMessages = 10;
+            queueClient = new QueueClient(ServiceBusConnectionString, QueueName);
+            Console.WriteLine("======================================================");
+            Console.WriteLine("Press ENTER key to exit after sending all the messages.");
+            Console.WriteLine("======================================================");
+
+            // Send messages.
+            await SendMessagesAsync(numberOfMessages);
+
+            Console.ReadKey();
+
+            await queueClient.CloseAsync();
+        }
+
+        private async static Task SendMessagesAsync(int numberOfMessagesToSend)
+        {
+            try
+            {
+                for (var i = 0; i < numberOfMessagesToSend; i++)
+                {
+                    // Create a new message to send to the queue.
+                    string messageBody = $"Message {i}";
+                    var message = new Message(Encoding.UTF8.GetBytes(messageBody));
+
+                    // Write the body of the message to the console.
+                    Console.WriteLine($"Sending message: {messageBody}");
+
+                    // Send the message to the queue.
+                    await queueClient.SendAsync(message);
+                }
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine($"{DateTime.Now} :: Exception: {exception.Message}");
+            }
+        }
+    }
+}
